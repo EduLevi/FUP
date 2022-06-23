0270492c389d123468b8ae8e1faed0a1f42b1764
EduLevi <102838231+EduLevi@users.noreply.github.com>
2022-06-23 11:25:08 -0300

    FUP

--- C/C - 4ª lista/atv.12/main.c
+++ C/C - 4ª lista/atv.12/main.c
@@ -8,14 +8,33 @@
 
 int main()
 {
-    int v1[4], v, i;
+    int v1[4], v = 0, vMaior = 0, vMenor = 0, i;
     for (i = 0; i < 5; i++)
     {
         scanf("%d", &v);
-        
-        
+        v1[i] = v;
     }
-
+    for (i = 0; i < 5; i++)
+    {
+        if(v1[i] > vMaior)
+        {
+            vMaior = i;
+            vMenor = i;
+        }
+    }
+    for (i = 0; i < 5; i++)
+    {
+        if(v1[i] < vMenor)
+        {
+            vMenor = i;
+        }
+    }
+    printf("\nMaior valor: %d", vMaior);
+    printf("\nMenor valor: %d", vMenor);
+    for (i = 0; i < 5; i++)
+    {
+        printf("\n%d", v1[i]);
+    }
     
     return 0;
 }
